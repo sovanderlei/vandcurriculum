a29f9415caede0c30ce20de324d41ace188d48ad
Vanderlei Soares de Oliveira <fs@e-deploy.local>
2021-05-23 23:28:56 -0300

    read data from file json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {Config} from '../app/model/config';
+import * as data_config from '../assets/filejsons/config.json';
 
 @Component({
   selector: 'app-root',
@@ -7,11 +8,12 @@ import {Config} from '../app/model/config';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'vandcurriculum';
+  title = 'Curriculum - Vanderlei Soares de Oliveira';
 
   config = new Config;
   constructor() {
 
+    console.log(data_config[0].Brasil.name);
     this.config = this.actualizes("BR");
 
   }
@@ -20,11 +22,16 @@ export class AppComponent {
   public actualizes(language: string){
 
     var config_temp = new Config;
+    var data_display = null;
+    if(language == "BR"){
+      data_display = data_config[0].Brasil;
+    }else{
+      data_display = data_config[0].English;
+    }
 
-
-    config_temp.name = "VANDERLEI S. OLIVEIRA";
-    config_temp.profession = "Engenheiro de Software";
-    config_temp.path_photo = "./assets/images/fotovand.jpeg";
+    config_temp.name = data_display.name;
+    config_temp.profession = data_display.profession;
+    config_temp.path_photo = data_display.path_photo;
 
     return config_temp;
 
